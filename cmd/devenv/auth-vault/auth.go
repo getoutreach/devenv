23b25b64ede2fc42fcac32f3e18216347c9e7d69
Peter Lisy <peter.lisy@outreach.io>
2022-01-18 16:07:38 -0800

    feat: auth-vault reauthenticates vault operator in devenv

---
+++ cmd/devenv/auth-vault/auth.go
@@ -0,0 +1,96 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/getoutreach/devenv/internal/vault"
+	"github.com/getoutreach/devenv/pkg/cmdutil"
+	"github.com/getoutreach/devenv/pkg/config"
+	"github.com/getoutreach/devenv/pkg/devenvutil"
+	"github.com/getoutreach/devenv/pkg/kube"
+	"github.com/getoutreach/gobox/pkg/box"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+//nolint:gochecknoglobals
+var (
+	authVault = `
+		auth-vault ensures Vault operator has valid credentials for creating/updating secrets.
+	`
+	authVaultExample = `
+		# Reauthenticate Vault operator
+		devenv auth-vault
+	`
+)
+
+type Options struct {
+	log  logrus.FieldLogger
+	k    kubernetes.Interface
+	conf *rest.Config
+
+	App string
+}
+
+func NewOptions(log logrus.FieldLogger) (*Options, error) {
+	k, conf, err := kube.GetKubeClientWithConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create kubernetes client")
+	}
+
+	return &Options{
+		k:    k,
+		conf: conf,
+		log:  log,
+	}, nil
+}
+
+func NewCmdAuthVault(log logrus.FieldLogger) *cli.Command {
+	return &cli.Command{
+		Name:        "auth-vault",
+		Usage:       "Reauthenticate Vault operator",
+		Description: cmdutil.NewDescription(authVault, authVaultExample),
+		Flags:       []cli.Flag{},
+		Action: func(c *cli.Context) error {
+			if c.Args().Len() == 0 {
+				return fmt.Errorf("missing application")
+			}
+			o, err := NewOptions(log)
+			if err != nil {
+				return err
+			}
+
+			o.App = c.Args().First()
+			return o.Run(c.Context)
+		},
+	}
+}
+
+func (o *Options) Run(ctx context.Context) error {
+	b, err := box.LoadBox()
+	if err != nil {
+		return errors.Wrap(err, "failed to load box configuration")
+	}
+
+	conf, err := config.LoadConfig(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to load config")
+	}
+
+	_, err = devenvutil.EnsureDevenvRunning(ctx, conf, b)
+	if err != nil {
+		return err
+	}
+
+	if b.DeveloperEnvironmentConfig.VaultConfig.Enabled {
+		if err := vault.EnsureLoggedIn(ctx, o.log, b, o.k); err != nil {
+			return errors.Wrap(err, "failed to refresh vault authentication")
+		}
+	}
+
+	return nil
+}
--- cmd/devenv/devenv.go
+++ cmd/devenv/devenv.go
@@ -20,6 +20,7 @@ import (
 
 	// Place any extra imports for your startup code here
 	///Block(imports)
+	"github.com/getoutreach/devenv/cmd/devenv/auth-vault"
 	"github.com/getoutreach/devenv/cmd/devenv/completion"
 	cmdcontext "github.com/getoutreach/devenv/cmd/devenv/context"
 	deleteapp "github.com/getoutreach/devenv/cmd/devenv/delete-app"
@@ -98,6 +99,7 @@ func main() {
 	}
 	app.Commands = []*cli.Command{
 		///Block(commands)
+		auth.NewCmdAuthVault(log),
 		provision.NewCmdProvision(log),
 		deployapp.NewCmdDeployApp(log),
 		deleteapp.NewCmdDeleteApp(log),
