dec366979ba8ef932d4a36c4f512be54933eaee3
Peter Lisy <peter.lisy@outreach.io>
2022-03-17 17:59:36 -0700

    feat: Added `apps dev` using `devspace` (#223)
    * fix(apps/deploy): correctly resolve legacy application names (#203)
    
    * fix: tags (#206)
    
    * fix: tags
    
    * local changes to apply
    
    Co-authored-by: Jared Allard <jaredallard@outlook.com>
    
    * feat: Add timeout to WaitForAllPodsToBeReady (#207)
    
    Co-authored-by: Ed Mangimelli <@outreach.io>
    
    * fix: resolve relative path when deleting (#197)
    
    * fix(gobox): update
    
    * chore(go): go mod tidy
    
    * fix: update gobox (#212)
    
    * fix(outlook): update embeded manifest to use cert-manager.io/v1 (#214)
    
    * feat(bootstrap): v9.14.5 (#215)
    
    Co-authored-by: bootstrapabot[bot] <97222+reactor-production[bot]@users.noreply.github.com>
    
    * fix: remove deprecation message as its too early (#218)
    
    * feat: Implemented `apps deploy` and `apps delete` using `devspace`
    
    * pr: doc fix
    
    * feat: Added `apps dev` using `devspace`
    
    * docs: updated todo comment
    
    * pr: fixed some todos and docs
    
    * pr: cleanup of merge
    
    * pr: more merge fixes
    
    * fix: Use prebuilt image as default with dev container
    
    Co-authored-by: Jared Allard <jaredallard@users.noreply.github.com>
    Co-authored-by: Jan Ludvik <89030648+jan-ludvik@users.noreply.github.com>
    Co-authored-by: Jared Allard <jaredallard@outlook.com>
    Co-authored-by: edmangimelli-outreach <89027551+edmangimelli-outreach@users.noreply.github.com>
    Co-authored-by: George Shaw <george.e.shaw.iv@outlook.com>
    Co-authored-by: Jared Allard <jared.allard@outreach.io>
    Co-authored-by: reactor-production[bot] <77807400+reactor-production[bot]@users.noreply.github.com>
    Co-authored-by: bootstrapabot[bot] <97222+reactor-production[bot]@users.noreply.github.com>
    Co-authored-by: Chris Robinson <chris.robinson@outreach.io>

---
+++ pkg/app/dev.go
@@ -0,0 +1,115 @@
+package app
+
+import (
+	"context"
+	"os"
+	"os/exec"
+
+	"github.com/getoutreach/devenv/internal/apps"
+	"github.com/getoutreach/devenv/pkg/kubernetesruntime"
+	"github.com/getoutreach/gobox/pkg/box"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+// Dev is a wrapper around NewApp().Dev()
+func Dev(ctx context.Context, log logrus.FieldLogger, k kubernetes.Interface, b *box.Config,
+	conf *rest.Config, appNameOrPath string, kr kubernetesruntime.RuntimeConfig) error {
+	app, err := NewApp(ctx, log, k, b, conf, appNameOrPath, &kr)
+	if err != nil {
+		return errors.Wrap(err, "parse app")
+	}
+	defer app.Close()
+
+	// We don't want to deploy the app from source, so we just immediately replace it with dev container.
+	app.Local = false
+	app.Version = "latest"
+
+	return app.Dev(ctx)
+}
+
+// DevStop is a wrapper around NewApp().DevStop()
+func DevStop(ctx context.Context, log logrus.FieldLogger, k kubernetes.Interface, b *box.Config,
+	conf *rest.Config, appNameOrPath string, kr kubernetesruntime.RuntimeConfig) error {
+	app, err := NewApp(ctx, log, k, b, conf, appNameOrPath, &kr)
+	if err != nil {
+		return errors.Wrap(err, "parse app")
+	}
+	defer app.Close()
+
+	// We don't want to deploy the app from source, so we just immediately replace it with dev container.
+	app.Local = false
+	app.Version = "latest"
+
+	return app.DevStop(ctx)
+}
+
+// devCommand returns the command that should be run to start the dev mode for the application.
+// There are two ways to start the dev mode:
+// 1. If there's an override script for the dev mode, we use that.
+// 2. If there's no override script, we use devspace dev directly.
+// We also check if devspace is able to start dev mode of the app (has dev configuration).
+func (a *App) devCommand(ctx context.Context) (*exec.Cmd, error) {
+	return a.command(ctx, &commandBuilderOptions{
+		requiredConfig: "dev",
+		devspaceArgs:   []string{"dev"},
+
+		fallbackCommandPaths: []string{"./scripts/devenv-apps-dev.sh"},
+		fallbackCommandArgs:  []string{"start"},
+	})
+}
+
+// devStopCommand returns the command that should be run to start the dev mode for the application.
+// There are two ways to start the dev mode:
+// 1. If there's an override script for the dev mode, we use that.
+// 2. If there's no override script, we use devspace reset pods directly.
+// We also check if devspace is able to start dev mode of the app (has dev configuration).
+func (a *App) devStopCommand(ctx context.Context) (*exec.Cmd, error) {
+	return a.command(ctx, &commandBuilderOptions{
+		requiredConfig: "dev",
+		devspaceArgs:   []string{"reset", "pods"},
+
+		fallbackCommandPaths: []string{"./scripts/devenv-apps-dev.sh"},
+		fallbackCommandArgs:  []string{"stop"},
+	})
+}
+
+// Dev starts the development mode for the application.
+func (a *App) Dev(ctx context.Context) error {
+	// TODO(DTSS-1496): Handle deleting jobs. devspace v6 will support doing this.
+
+	cmd, err := a.devCommand(ctx)
+	if err != nil {
+		return err
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err = cmd.Run(); err != nil {
+		return errors.Wrap(err, "failed to start dev mode for the application")
+	}
+
+	return a.appsClient.Set(ctx, &apps.App{Name: a.RepositoryName, Version: a.Version})
+}
+
+// Dev stop the development mode for the application.
+func (a *App) DevStop(ctx context.Context) error {
+	cmd, err := a.devStopCommand(ctx)
+	if err != nil {
+		return err
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err = cmd.Run(); err != nil {
+		return errors.Wrap(err, "failed to stop dev mode for the application")
+	}
+
+	return a.appsClient.Set(ctx, &apps.App{Name: a.RepositoryName, Version: a.Version})
+}
